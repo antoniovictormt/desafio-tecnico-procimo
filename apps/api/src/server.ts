45f291bedca9ba268d293d824e43009aa3b6034a
antonio-victor <antoniovictormt@gmail.com>
2025-12-15 10:12:59 -0300

    feat(api): implement real-time public chat with websocket

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,12 +1,45 @@
 import express from "express"
-import routes from "./routes"
+import http from "http"
 import cors from "cors"
+import { Server } from "socket.io"
+import path from "path"
+import { initialMessages } from "./seed/message"
 
 const app = express()
-
 app.use(cors())
-app.use(routes)
 
-app.listen(3333, () => {
-    console.log("Server running!")
+const httpServer = http.createServer(app)
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: "*"
+    }
+})
+
+app.use(express.static(path.resolve(__dirname, "..", "public")))
+
+let messages = initialMessages
+
+io.on("connection", socket => {
+    console.log("Client connected:", socket.id)
+
+    console.log(messages)
+
+    socket.emit("previousMessage", messages)
+
+    socket.on("message", message => {
+        const normalizedMessage = {
+            id: crypto.randomUUID(),
+            user: message.user,
+            content: message.content,
+            timestamp: Date.now()
+        }
+
+        messages.push(normalizedMessage)
+        socket.broadcast.emit("receivedMessage", normalizedMessage)
+    })
+})
+
+httpServer.listen(3333, () => {
+    console.log("🚀 Server running on http://localhost:3333")
 })
